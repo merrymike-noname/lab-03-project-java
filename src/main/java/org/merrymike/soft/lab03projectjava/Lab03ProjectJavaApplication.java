3e5a26af0c20e3193eb03e5fe6d9ecdda9f095a0
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2024-04-03 21:28:26 +0300

    Initial commit

---
+++ src/main/java/org/merrymike/soft/lab03projectjava/Lab03ProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package org.merrymike.soft.lab03projectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab03ProjectJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab03ProjectJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/merrymike/soft/lab03projectjava/Lab03ProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.merrymike.soft.lab03projectjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab03ProjectJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
